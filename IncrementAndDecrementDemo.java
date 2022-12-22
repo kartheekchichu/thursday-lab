842c7d62cb5b9a82b7b6f02c925dd07a49282782
kartheekchichu <121236951+kartheekchichu@users.noreply.github.com>
2022-12-22 18:27:33 +0530

    Add files via upload

---
+++ IncrementAndDecrementDemo.java
@@ -0,0 +1,11 @@
+package students;
+
+public class IncrementAndDecrementDemo {
+
+	public static void main(String[] args) {
+		int x=10,y;
+		y=x--;
+		System.out.println("x="+x+" Y="+y);
+		}
+	}
+
